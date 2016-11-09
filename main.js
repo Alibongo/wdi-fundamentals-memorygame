08d316fa17b2545f3829c782a6ff7e38944aa63d
Alibongo <oioimolloy@gmail.com>
2016-11-10 10:11:45 +1030

    created pop-ups

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+
+// original console notification
 console.log("JS file is connected to HTML! Woo!")
+//these are the cards
 var cardOne = "queen";
 var cardTwo  = "queen";
 var cardThree = "king";
 var cardFour = "king";
+//matching queens
+if (cardOne === cardTwo){
+alert("You found a match");
+}else {
+	alert ("Sorry, try again");
+}
+//matching kings
+if (cardThree ===cardFour){
+alert("You found a match");
+}else {
+	alert ("Sorry, try again");
+}
+//if matching mixed
+if (cardThree ===cardTwo){
+alert("You found a match");
+}else {
+	alert ("Sorry, try again");
+}
+//if matching mixed
+if (cardFour ===cardOne){
+alert("You found a match");
+}else {
+	alert ("Sorry, try again");
+}
+
