2a33ee5bab866eba550ad041d83706ae691cf43a
Alibongo <oioimolloy@gmail.com>
2016-11-17 10:08:34 +1030

    final assignment

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 // original console notification
 console.log("got this working!")
 //My cards
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo  = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
+
+
+
 //matching queens
 /*if (cardOne === cardTwo){
 alert("You found a match");
@@ -30,11 +33,26 @@ alert("You found a match");
 }else {
 	alert ("Sorry, try again");
 }*/
+
+
+//game card array
+var cards = ['king', 'king', 'queen', 'queen'];
+
+//cards in play
+var cardsinPlay = [];
+//last session's work is wrapped in createBoard
+function createBoard(){
+//the board game
 var board = document.getElementById('game-board');
 var createCards = function(){
-	for (var i = 0; i<4; i++){
+	//this is now changed to length to avoid fixed number
+	for (var i = 0; i<cards.length; i++){
 		var cardElement =document.createElement('div');
 		cardElement.className ='card';
+		//sets a 'data card'to the element of the array. Stores data about element that's not tied to a style.
+		cardElement.setAttribute('data-card', cards[i]);
+		// when card is clicked the function isTwoCards will be executed
+    cardElement.addEventListener('click', isTwoCards)
 		board.appendChild(cardElement);
 		console.log('the board: ', board);
    console.log('the cardElement: ', cardElement);
